99620b9544ed25f0bb9fe67419ab5b7f5fc156d8
superyarick <yarick@yarick.com>
2017-12-13 22:32:37 -0500

    Added resource creation   - s3_bucket public object   - s3_bucket public authenticated object   - s3_bucket private object

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ resource "aws_instance" "example" {
 resource "aws_s3_bucket" "aws_demo_bucket" {
   bucket = "${var.aws_ssh_key_name}-aws_demo_bucket"
   acl    = "public-read"
+  force_destroy = true
 
   tags {
     Name        = "aws_demo_bucket"
@@ -25,20 +26,39 @@ resource "aws_s3_bucket" "aws_demo_bucket" {
 
 # add s3 bucket elements - pub
 
-resource "aws_s3_bucket_object" "object" {
+resource "aws_s3_bucket_object" "public-read" {
   bucket = "${var.aws_ssh_key_name}-aws_demo_bucket"
-  acl    = "public"
-  key    = "public-pic.jpg"
+  acl    = "public-read"
+  key    = "public-pic-read.jpg"
   source = "./data/public-pic.jpg"
   etag   = "${md5(file("./data/public-pic.jpg"))}"
+
+  depends_on = ["aws_s3_bucket.aws_demo_bucket"]
 }
 
+# add s3 bucket elements - pub Authenticated Users only
+
+resource "aws_s3_bucket_object" "authenticated-read" {
+  bucket = "${var.aws_ssh_key_name}-aws_demo_bucket"
+  acl    = "authenticated-read"
+  key    = "public-pic-authenticated.jpg"
+  source = "./data/public-pic.jpg"
+  etag   = "${md5(file("./data/public-pic.jpg"))}"
+
+  depends_on = ["aws_s3_bucket.aws_demo_bucket"]
+}
 # add s3 bucket elements - pri
 
-resource "aws_s3_bucket_object" "object" {
+resource "aws_s3_bucket_object" "private" {
   bucket = "${var.aws_ssh_key_name}-aws_demo_bucket"
   acl = "private"
   key    = "private-pic.jpg"
   source = "./data/private-pic.jpg"
   etag   = "${md5(file("./data/private-pic.jpg"))}"
+
+  depends_on = ["aws_s3_bucket.aws_demo_bucket"]
 }
+
+# valid ACLs are Error: aws_s3_bucket_object.public: "acl" contains an invalid canned ACL type "public". V
+#alid types are either "authenticated-read", "aws-exec-read", "bucket-owner-full-control", "bucket-owner-read",
+# "private", "public-read", or "public-read-write"

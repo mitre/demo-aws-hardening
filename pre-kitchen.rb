641e28d623c3f2781b19aba5ea91efc7fedc08c8
Yarick <yarick@yarick.com>
2018-01-04 11:49:20 -0500

    added check for default VPC in auxillary script - pre-kitchen.rb
    Signed-off-by: Yarick <yarick@yarick.com>

--- pre-kitchen.rb
+++ pre-kitchen.rb
@@ -19,3 +19,29 @@ puts ''
 puts '----------'
 puts 'You are OK to run Test Kitchen' if @run_kitchen == true
 puts 'Please SET the above Envrioment variables before running kitchen' if @run_kitchen == false
+
+require 'rubygems'; require 'json'; 
+# puts  %x[aws ec2 describe-vpcs]
+
+# puts JSON.pretty_generate(JSON[STDIN.read]);"
+# puts JSON.pretty_generate(['CidrBlock: 172.31.0.0/16']);
+# puts '*****'
+
+my_vpcs =  JSON.parse(%x[aws ec2 describe-vpcs]);
+
+# puts my_vpcs['Vpcs'][0]['IsDefault']
+
+re = Regexp.union('false')
+if my_vpcs['Vpcs'][0]['IsDefault'].to_s.match(re)
+	puts 'Default VPC does NOT exist'
+	puts '  - please use an account/region with default VPC'
+	puts '  - alternativelly,  consider hard-coding default vpc with terraform'
+	puts '		set aws_default_vpc=' + my_vpcs['Vpcs'][0]['VpcId']
+else
+	puts 'Default VPC Exists'
+end
+
+# puts '-----'
+# my_vpc = my_vpcs['Vpcs']
+# puts '+++'
+# puts my_vpc[0]

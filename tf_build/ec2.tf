e6534e5ef591b342938d4ce2fe367fa890324063
Matthew Dromazos <dromazmj@dukes.jmu.edu>
2018-01-09 18:31:33 -0500

    Fixes default security group resource
    Signed-off-by: Matthew Dromazos <dromazmj@dukes.jmu.edu>
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- tf_build/ec2.tf
+++ tf_build/ec2.tf
@@ -23,12 +23,16 @@ data "aws_vpc" "default" {
   default = "true"
 }
 
-resource "aws_default_security_group" "default_" {
+resource "aws_default_security_group" "default" {
   vpc_id = "${data.aws_vpc.default.id}"
 }
 
 output "ec2_security_group_default_vpc_id" {
   value = "${data.aws_vpc.default.id}"
+}
+
+output "ec2_security_group_default_group_id" {
+  value = "${aws_default_security_group.default.id}"
 }
 
 # Create ingress rules for specified security group
